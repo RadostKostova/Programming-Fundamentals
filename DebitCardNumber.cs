71820f87a467b9cf59d63b78e72350a80e4a2da0
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-03-04 14:37:04 +0200

    Programming-Fundamentals

---
+++ DebitCardNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DebitCardNumber
+{
+    class DebitCardNumber
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstNum:d4} {secNum:d4} {thirdNum:d4} {fourthNum:d4}");
+        }
+    }
+}
