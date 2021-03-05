6dd361e1c3106cca06e2a206edc50f58b9d9401c
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-03-05 18:03:09 +0200

    Programming-Fundamentals

---
+++ IntroAndBasicSymtaxExercises/BeverageLabels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BeverageLabels
+{
+    class BeverageLabels
+    {
+        static void Main(string[] args)
+        {
+            string foodName = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            double kcalIn100ml = double.Parse(Console.ReadLine());
+            double sugarsIn100ml = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{volume}ml {foodName}:");
+            Console.WriteLine($"{((kcalIn100ml * volume) / 100)}kcal, {((sugarsIn100ml * volume) / 100)}g sugars");
+        }
+    }
+}
