2aeb3592844ca0f2f6f989f3a93ee1fc4cf8070d
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-03-20 07:08:17 +0200

    Programming-Fundamentals

---
+++ BasicSyntaxConditionalStatementsAndLoopsLab/BackIn30Minutes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BackIn30Minutes
+{
+    class BackIn30Minutes
+    {
+        static void Main(string[] args)
+        {
+            int hourInput = int.Parse(Console.ReadLine());
+            int minuteInput = int.Parse(Console.ReadLine());
+
+            minuteInput += 30;
+
+            if (minuteInput > 59)
+            {
+                hourInput++;
+                minuteInput -= 60;
+            }
+
+            if (hourInput > 23)
+            {
+                hourInput = 0;
+            }
+
+            Console.WriteLine($"{hourInput:D1}:{minuteInput:D2}");
+        }
+    }
+}
