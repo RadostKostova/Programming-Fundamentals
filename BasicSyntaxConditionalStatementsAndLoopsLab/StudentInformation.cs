8973ece760b82681bb3375cf1b47e19d048944f5
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-03-25 17:17:35 +0200

    Programming-Fundamentals

---
+++ BasicSyntaxConditionalStatementsAndLoopsLab/StudentInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StudentInformation
+{
+    class StudentInformation
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double age = double.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+
+            string output = string.Format($"Name: {name}, Age: {age}, Grade: {grade:f2}");
+            Console.WriteLine(output);
+        }
+    }
+}
