e6d5275b81b386fff8505e1aa19aaff776afeeda
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-03-25 17:18:36 +0200

    Programming-Fundamentals

---
+++ BasicSyntaxConditionalStatementsAndLoopsLab/SumOfOddNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    class SumOfOddNumbers
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= numbers * 2; i += 2)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
