983a793e237d9679b2e7217e7ed44b1c4685684e
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-03-27 16:01:40 +0200

    Programming-Fundamentals

---
+++ BasicSyntaxConditionalStatementsAndLoopsLab/TheatrePromotion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace TheatrePromotion
+{
+    class TheatrePromotion
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine().ToLower();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+
+            if (age >= 0 && age <= 122)
+            {
+                switch (typeOfDay)
+                {
+                    case "weekday":
+                        if (0 <= age && age <= 18)
+                        {
+                            price = 12;
+                            Console.WriteLine($"{price}$");
+                        }
+                        else if (18 < age && age <= 64)
+                        {
+                            price = 18;
+                            Console.WriteLine($"{price}$");
+                        }
+                        else if (64 < age && age <= 122)
+                        {
+                            price = 12;
+                            Console.WriteLine($"{price}$");
+                        }
+                        break;
+                    case "weekend":
+                        if (0 <= age && age <= 18)
+                        {
+                            price = 15;
+                            Console.WriteLine($"{price}$");
+                        }
+                        else if (18 < age && age <= 64)
+                        {
+                            price = 20;
+                            Console.WriteLine($"{price}$");
+                        }
+                        else if (64 < age && age <= 122)
+                        {
+                            price = 15;
+                            Console.WriteLine($"{price}$");
+                        }
+                        break;
+                    case "holiday":
+                        if (0 <= age && age <= 18)
+                        {
+                            price = 5;
+                            Console.WriteLine($"{price}$");
+                        }
+                        else if (18 < age && age <= 64)
+                        {
+                            price = 12;
+                            Console.WriteLine($"{price}$");
+                        }
+                        else if (64 < age && age <= 122)
+                        {
+                            price = 10;
+                            Console.WriteLine($"{price}$");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Error!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
