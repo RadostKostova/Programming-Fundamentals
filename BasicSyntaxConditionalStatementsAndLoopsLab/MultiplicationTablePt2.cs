51a44259f13c78f84317b9f3e90320c671c99a17
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-03-29 12:56:28 +0300

    Programming-Fundamentals

---
+++ BasicSyntaxConditionalStatementsAndLoopsLab/MultiplicationTablePt2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MultiplicationTablePt2
+{
+    class MultiplicationTablePt2
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            if (multiplier > 10)
+            {
+                Console.WriteLine($"{number} X {multiplier} = {number * multiplier}");
+            }
+            else
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+                    sum = number * i;
+                    Console.WriteLine($"{number} X {i} = {sum}");
+                    sum = 0;
+                }
+            }
+        }
+    }
+}
