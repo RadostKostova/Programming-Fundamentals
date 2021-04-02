51ef4a21021d0257c0a488cb5fc25128f7bdd4cf
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-02 14:44:01 +0300

    Programming-Fundamentals

---
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/Login.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Login
+{
+    class Login
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                string inputPassword = Console.ReadLine();
+
+                if (password == inputPassword)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+            }
+
+            string lastTryPassword = Console.ReadLine();
+
+            if (lastTryPassword == password)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }
+            else
+            {
+                Console.WriteLine($"User {username} blocked!");
+            }
+        }
+    }
+}
