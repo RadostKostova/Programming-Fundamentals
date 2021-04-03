63b939b136598143468e18b1d8cb5a9a8e774f80
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-03 21:22:32 +0300

    Programming-Fundamentals

---
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/StrongNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StrongNumber
+{
+    class StrongNumber
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            int copyNum = inputNum;
+            int sum = 0;
+
+            while (copyNum != 0)
+            {
+                int currDigit = copyNum % 10;
+                int currFactorial = 1;
+
+                for (int i = 1; i <= currDigit; i++)
+                {
+                    currFactorial *= i;
+                }
+                sum += currFactorial;
+
+                copyNum /= 10;
+            }
+
+            if (sum == inputNum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
