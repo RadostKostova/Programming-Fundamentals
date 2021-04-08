f6a6bf4ecc6c8591082f175008b2b7f6c5d116a6
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-08 08:51:06 +0300

    Programming-Fundamentals

---
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/RageExpenses.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RageExpenses
+{
+    class RageExpenses
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+            double totalCost = 0;
+
+            for (int i = 2; i <= lostGames; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    totalCost += headsetPrice;
+                }
+
+                if (i % 3 == 0)
+                {
+                    totalCost += mousePrice;
+                }
+
+                if (i % 6 == 0)
+                {
+                    totalCost += keyboardPrice;
+                    if (i % 12 == 0)
+                    {
+                        totalCost += displayPrice;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Rage expenses: {totalCost:f2} lv.");
+        }
+    }
+}
