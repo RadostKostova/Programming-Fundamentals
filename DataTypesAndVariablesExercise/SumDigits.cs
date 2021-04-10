ba535f87dad5d941e93e4d87864dbde48b7f4365
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-10 07:14:58 +0300

    Programming-Fundamentals

---
+++ DataTypesAndVariablesExercise/SumDigits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currNumber = (int)char.GetNumericValue(input[i]);
+
+                sum += currNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
