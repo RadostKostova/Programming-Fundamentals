5f0aee71f69636b9712b3142859d803ca6e8739d
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-12 08:56:14 +0300

    Programming-Fundamentals

---
+++ DataTypesAndVariablesExercise/SumOfChars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalChars = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < totalChars; i++)
+            {
+                int symbol = char.Parse(Console.ReadLine());
+
+                sum += symbol;
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
