f43fb35a89f4a1ab1be0fdac4119781e325ba8e5
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-14 07:44:22 +0300

    Programming-Fundamentals

---
+++ DataTypesAndVariablesExercise/TriplesOfLatinLetters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input; i++)
+            {
+                for (int k = 0; k < input; k++)
+                {
+                    for (int j = 0; j < input; j++)
+                    {
+                        char first = (char)('a' + i);
+                        char second = (char)('a' + k);
+                        char third = (char)('a' + j);
+
+                        Console.WriteLine($"{first}{second}{third}");
+                    }
+                }
+            }
+        }
+    }
+}
