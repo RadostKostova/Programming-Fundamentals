2f8134340195a791fc10050bf5185f961e9c2a24
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-16 12:20:16 +0300

    Programming-Fundamentals

---
+++ DataTypesAndVariablesExercise/BeerKegs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalKegs = int.Parse(Console.ReadLine());
+            string model = string.Empty;
+
+            double kegInfo = 0;
+
+            for (int i = 0; i < totalKegs; i++)
+            {
+                string currModel = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double currKegInfo = Math.PI * Math.Pow(radius, 2) * height;
+
+                if (currKegInfo > kegInfo)
+                {
+                    kegInfo = currKegInfo;
+                    model = currModel;
+                }
+            }
+            Console.WriteLine(model);
+        }
+    }
+}
