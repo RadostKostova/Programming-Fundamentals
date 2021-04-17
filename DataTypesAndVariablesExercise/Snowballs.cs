476e13ad9c3962ba778e9a1d754fa77d33956e5a
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-17 20:41:29 +0300

    Programming-Fundamentals

---
+++ DataTypesAndVariablesExercise/Snowballs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+namespace Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger maxValue = -1;
+            string result = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                int snow = int.Parse(Console.ReadLine());
+                int time = int.Parse(Console.ReadLine());
+                int quality = int.Parse(Console.ReadLine());
+
+                BigInteger value = BigInteger.Pow((snow / time), quality);
+
+                if (value > maxValue)
+                {
+                    result = ($"{snow} : {time} = {value} ({quality})");
+                    maxValue = value;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
