16ec4e2af64b072f03a438a0f039ffc10cb364ec
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-19 01:10:29 +0300

    Programming-Fundamentals

---
+++ DataTypesAndVariablesExercise/SpiceMustFlow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startYeald = int.Parse(Console.ReadLine());
+            int spicesMined = 0;
+            int dropdownYeald = 10;
+            int workersConsumer = 26;
+            int days = 0;
+
+            while (startYeald >= 100)
+            {
+                spicesMined += startYeald;
+                spicesMined -= workersConsumer;
+                startYeald -= dropdownYeald;
+                days++;
+            }
+
+            if (spicesMined != 0)
+            {
+                spicesMined -= workersConsumer;
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(spicesMined);
+        }
+    }
+}
