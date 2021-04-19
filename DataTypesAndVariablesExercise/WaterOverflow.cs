f71b94e115f02f735863890c5034e5a81a74b300
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-20 01:43:20 +0300

    Programming-Fundamentals

---
+++ DataTypesAndVariablesExercise/WaterOverflow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            int waterTank = 0;
+
+            int capacity = 255;
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                int litresToPour = int.Parse(Console.ReadLine());
+
+                if (waterTank + litresToPour <= capacity)
+                {
+                    waterTank += litresToPour;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+
+            }
+            Console.WriteLine(waterTank);
+        }
+    }
+}
