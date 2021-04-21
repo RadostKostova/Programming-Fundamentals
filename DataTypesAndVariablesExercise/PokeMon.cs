662a39dedd2c63ea37c5a81a000c7b6ca278e692
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-21 11:41:06 +0300

    Programming-Fundamentals

---
+++ DataTypesAndVariablesExercise/PokeMon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());                //N
+            int distance = int.Parse(Console.ReadLine());             //M
+            int exhaustion = int.Parse(Console.ReadLine());           //Y
+
+            int target = 0;
+            double tempPower = power * 0.5;
+
+            while (power >= distance)
+            {
+                target++;
+                power -= distance;
+
+
+                if (power == tempPower && exhaustion != 0)
+                {
+                    power /= exhaustion;
+                }
+            }
+
+            Console.WriteLine(power);
+            Console.WriteLine(target);
+        }
+    }
+}
