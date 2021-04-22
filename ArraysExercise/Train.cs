d73e99f75c946fd95ddeaac2c05908fd7a85c30d
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-22 18:28:43 +0300

    Programming-Fundamentals

---
+++ ArraysExercise/Train.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            int[] passangers = new int[wagons];
+
+            int sumPassangers = 0;
+
+            for (int i = 0; i < wagons; i++)
+            {
+                passangers[i] = int.Parse(Console.ReadLine());
+                sumPassangers += passangers[i];
+
+            }
+            Console.WriteLine(string.Join(' ', passangers));
+            Console.WriteLine(sumPassangers);
+        }
+    }
+}
