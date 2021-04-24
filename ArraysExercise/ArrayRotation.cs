7a032a642a0f204124d66cb7c9e27a202fb4ab53
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-04-24 10:14:58 +0300

    Programming-Fundamentals

---
+++ ArraysExercise/ArrayRotation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console
+                .ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotations; i++)
+            {
+                int temp = array[0];
+
+                for (int index = 0; index < array.Length - 1; index++)
+                {
+                    array[index] = array[index + 1];
+                }
+
+                array[array.Length - 1] = temp;
+            }
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
