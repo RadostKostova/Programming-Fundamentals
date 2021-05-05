b72bf562b0ad90bb1f59e0f5733aeaafbfe8449e
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-06 01:26:53 +0300

    Programming-Fundamentals

---
+++ ArraysExercise/MagicSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console
+                .ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int magicNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int firstNumber = array[i];
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    int secondNumber = array[j];
+
+                    if (firstNumber + secondNumber == magicNum)
+                    {
+                        Console.WriteLine($"{firstNumber} {secondNumber}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
