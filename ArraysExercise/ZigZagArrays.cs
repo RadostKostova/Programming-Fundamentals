d224bd3370777375027618d046a2a6af63913e1a
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-07 10:29:13 +0300

    Programming-Fundamentals

---
+++ ArraysExercise/ZigZagArrays.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace ZigZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            string[] firstArr = new string[numbers];
+            string[] secondArr = new string[numbers];
+
+            for (int i = 0; i < numbers; i++)
+            {
+                string[] currArr = Console.ReadLine().Split(" ").ToArray();
+
+                string zeroElement = currArr[0];
+                string oneElement = currArr[1];
+
+                if (i % 2 == 0)
+                {
+                    firstArr[i] = zeroElement;
+                    secondArr[i] = oneElement;
+                }
+                else
+                {
+                    firstArr[i] = oneElement;
+                    secondArr[i] = zeroElement;
+                }
+            }
+            Console.WriteLine(string.Join(" ", firstArr));
+            Console.WriteLine(string.Join(" ", secondArr));
+        }
+    }
+}
