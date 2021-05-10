0c29bc319b25a5205eb5f627a1431a9dc977c0bb
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-10 09:49:51 +0300

    Programming-Fundamentals

---
+++ ArraysExercise/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console
+                .ReadLine()
+                .Split(" ");
+
+            int bestCount = 0;
+            int bestIndex = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string currElement = array[i];
+                int currCount = 1;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (currElement == array[j])
+                    {
+                        currCount++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (currCount > bestCount)
+                {
+                    bestCount = currCount;
+                    bestIndex = i;
+                }
+            }
+
+            for (int i = 0; i < bestCount; i++)
+            {
+                Console.Write(array[bestIndex] + " ");
+            }
+        }
+    }
+}
