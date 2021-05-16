2290a94e73c271052d68009bc02b82d2a163368d
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-16 12:18:14 +0300

    Programming-Fundamentals

---
+++ MethodsExercise/AddAndSubtract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SumResult(num1, num2, num3));
+        }
+
+        private static int SumResult(int num1, int num2, int num3)
+        {
+            int sum = num1 + num2;
+            sum -= num3;
+
+            return sum;
+        }
+    }
+}
