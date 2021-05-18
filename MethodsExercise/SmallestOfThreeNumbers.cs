d5b758979bc8f793af09f4f1fafaff224fa9e905
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-18 10:28:27 +0300

    Programming-Fundamentals

---
+++ MethodsExercise/SmallestOfThreeNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = int.Parse(Console.ReadLine());
+
+                int result = GetSmallerNum(a, b, c);
+                Console.WriteLine(result);
+            }
+        }
+
+        private static int GetSmallerNum(int a, int b, int c)
+        {
+            if (a < b && a < c)
+            {
+                int result = a;
+                return result;
+            }
+            else if (b < a && b < c)
+            {
+                int result = b;
+                return result;
+            }
+            else
+            {
+                int result = c;
+                return result;
+            }
+        }
+    }
+}
