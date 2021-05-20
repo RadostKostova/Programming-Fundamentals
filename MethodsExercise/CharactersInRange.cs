f7e38bc16cca9775633314a7f57f9b7fd1c6bbb2
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-20 11:51:41 +0300

    Programming-Fundamentals

---
+++ MethodsExercise/CharactersInRange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char sym1 = char.Parse(Console.ReadLine());
+            char sym2 = char.Parse(Console.ReadLine());
+
+            PrintBetweenSymbols(sym1, sym2);
+        }
+
+        private static void PrintBetweenSymbols(char start, char end)
+        {
+            if (start > end)
+            {
+                for (int i = ++end; i < start; i++)
+                {
+                    Console.Write((char)i + " ");
+                }
+            }
+            else
+            {
+                for (int i = ++start; i < end; i++)
+                {
+                    Console.Write((char)i + " ");
+                };
+            }
+        }
+    }
+}
