6f37e80bc82c41fd5ba6d1f9b9fbb368aa27c226
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-21 10:00:42 +0300

    Programming-Fundamentals

---
+++ MethodsExercise/MiddleCharacters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            PrintMiddleChar(input);
+        }
+
+        private static void PrintMiddleChar(string input)
+        {
+            if (input.Length % 2 == 1)
+            {
+                Console.WriteLine(input[input.Length / 2]);
+            }
+            else
+            {
+                Console.WriteLine($"{input[input.Length / 2 - 1]}{input[input.Length / 2]}");
+            }
+        }
+    }
+}
