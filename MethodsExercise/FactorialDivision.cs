432ba6cd035d799550ef10c6116ca78650d8aec8
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-22 11:16:22 +0300

    Programming-Fundamentals

---
+++ MethodsExercise/FactorialDivision.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal firstNumber = decimal.Parse(Console.ReadLine());
+            decimal secondNumber = decimal.Parse(Console.ReadLine());
+
+            decimal first = GetFactorial(firstNumber);
+            decimal second = GetFactorial(secondNumber);
+
+            Console.WriteLine($"{first / second:F2}");
+        }
+
+        private static decimal GetFactorial(decimal number)
+        {
+            decimal factorial = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
