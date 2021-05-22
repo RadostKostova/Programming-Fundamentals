8b59e1c6e139d1be0db6006454d6d92736f722c6
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-23 00:35:45 +0300

    Programming-Fundamentals

---
+++ MethodsExercise/TopNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            PrintTopNumber(number);
+        }
+
+        private static void PrintTopNumber(int number)
+        {
+            for (int i = 0; i <= number; i++)
+            {
+                string currNumber = i.ToString();
+                bool isOddDigit = false;
+                int sumOfDigigts = 0;
+
+                foreach (var curr in currNumber)
+                {
+                    int parseNumber = (int)curr;
+
+                    if (parseNumber % 2 == 1)
+                    {
+                        isOddDigit = true;
+                    }
+
+                    sumOfDigigts += parseNumber;
+                }
+
+                if (sumOfDigigts % 8 == 0 && isOddDigit)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
