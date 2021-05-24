5f2a019d11221cb38e1205fc719b5d1b939d46cd
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-25 01:14:15 +0300

    Programming-Fundamentals

---
+++ MethodsExercise/VowelsCount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            int countVowels = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currSymbol = input[i];
+
+                if (currSymbol == 'a'
+                    || currSymbol == 'o'
+                    || currSymbol == 'u'
+                    || currSymbol == 'e'
+                    || currSymbol == 'i'
+                    || currSymbol == 'y')
+                {
+                    countVowels++;
+                }
+            }
+
+            GetVowels(countVowels);
+            Console.WriteLine(countVowels);
+        }
+
+        private static int GetVowels(int countVowels)
+        {
+            return countVowels; //?
+        }
+    }
+}
