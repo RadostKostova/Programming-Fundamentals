3a6d08b006b89d0b078506372da29a08cf3a4f33
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-26 02:04:33 +0300

    Programming-Fundamentals

---
+++ MethodsExercise/NxNMatrix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                PrintInputXInput(input);
+            }
+
+            static void PrintInputXInput(int input)
+            {
+                for (int row = 0; row < input; row++)
+                {
+                    for (int col = 0; col < input; col++)
+                    {
+                        Console.Write(input + " ");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
