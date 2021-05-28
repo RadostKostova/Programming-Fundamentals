a62c38197c330f4b5bcaf0b385bb43d795490be3
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-05-28 10:10:53 +0300

    Programming-Fundamentals

---
+++ ListsExercise/ChangeList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] cmdArgs = command.Split();
+                string firstCommand = cmdArgs[0];
+                int element = int.Parse(cmdArgs[1]);
+
+                if (firstCommand == "Delete")
+                {
+                    list.RemoveAll(x => x == element);
+                }
+                else if (firstCommand == "Insert")
+                {
+                    int index = int.Parse(cmdArgs[2]);
+                    list.Insert(index, element);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
