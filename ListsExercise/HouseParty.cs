172225cba9ec84a04cf878db17664ba9b602b035
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-06-01 00:07:39 +0300

    Programming-Fundamentals

---
+++ ListsExercise/HouseParty.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            List<string> guests = new List<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split();
+                string guestName = cmdArgs[0];
+
+                if (cmdArgs.Length > 3)
+                {
+                    if (guests.Contains(guestName))
+                    {
+                        guests.Remove(guestName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{guestName} is not in the list!");
+                    }
+                }
+                else
+                {
+                    if (!guests.Contains(guestName))
+                    {
+                        guests.Add(guestName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{guestName} is already in the list!");
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, guests));
+        }
+    }
+}
