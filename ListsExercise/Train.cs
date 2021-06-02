8578f09440c941dcb0a0a29841f1d1d3600bf803
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-06-03 02:16:42 +0300

    Programming-Fundamentals

---
+++ ListsExercise/Train.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int capacity = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] cmdArgs = command.Split();
+
+                if (cmdArgs[0] == "Add")
+                {
+                    wagons.Add(int.Parse(cmdArgs[1]));
+                }
+                else
+                {
+                    int passangers = int.Parse(cmdArgs[0]);
+
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        int currWagon = wagons[i];
+                        bool isEnoughSpace = (currWagon + passangers) <= capacity;
+
+                        if (isEnoughSpace)
+                        {
+                            wagons[i] += passangers;
+                            break;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
