abf4fa5fb267a0eb404bf5b6a344d69f331eb657
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-06-16 12:51:26 +0300

    Programming-Fundamentals

---
+++ AssociativeArraysExercise/CountCharsInAString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> occurences = new Dictionary<char, int>();
+
+            foreach (var letter in input)
+            {
+                if (letter != ' ')
+                {
+                    if (occurences.ContainsKey(letter) == false)
+                    {
+                        occurences.Add(letter, 0);
+                    }
+                    occurences[letter]++;
+                }
+            }
+
+            foreach (var chars in occurences)
+            {
+                char currKey = chars.Key;
+                int currValue = chars.Value;
+
+                Console.WriteLine($"{currKey} -> {currValue}");
+            }
+        }
+    }
+}
