482573009a9b5fa1ea9bde506ec3eeda585eb379
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-06-19 01:37:08 +0300

    Programming-Fundamentals

---
+++ AssociativeArraysExercise/StudentAcademy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pairs = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> studentsAndGrades = new Dictionary<string, List<double>>();
+
+            while (pairs-- > 0)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (studentsAndGrades.ContainsKey(studentName) == false)
+                {
+                    studentsAndGrades.Add(studentName, new List<double>());
+                }
+                studentsAndGrades[studentName].Add(grade);
+            }
+
+            studentsAndGrades = studentsAndGrades
+                .Where(x => x.Value.Average() >= 4.5)
+                .OrderByDescending(x => x.Value.Average())
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            Console.WriteLine(string.Join(Environment.NewLine, studentsAndGrades
+                .Select(x => $"{x.Key} -> {x.Value.Average():f2}")));
+        }
+    }
+}
