d7ecadbeaddefd0d5b60a5f10ea1ea2e7eb20a9d
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-07-06 09:19:36 +0300

    Programming-Fundamentals

---
+++ TextProcessingExercise/ExtractFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pathFile = Console.ReadLine().Split("\\").ToArray();
+
+            string lastFile = pathFile[pathFile.Length - 1];
+
+            var array = lastFile.Split(".");
+            var fileName = array[0];
+            var extension = array[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
