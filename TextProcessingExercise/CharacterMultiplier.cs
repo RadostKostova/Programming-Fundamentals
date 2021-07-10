481227a111777c45b5595820c025b6821df71141
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-07-10 15:42:36 +0300

    Programming-Fundamentals

---
+++ TextProcessingExercise/CharacterMultiplier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string first = tokens[0];
+            string second = tokens[1];
+
+            var longestWord = first;
+            var shortestWord = second;
+
+            if (first.Length < second.Length)
+            {
+                longestWord = second;
+                shortestWord = first;
+            }
+
+            var total = CharManipulator(longestWord, shortestWord);
+            Console.WriteLine(total);
+        }
+
+        public static int CharManipulator(string longestWord, string shortestWord)
+        {
+            var sum = 0;
+
+            for (int i = 0; i < shortestWord.Length; i++)
+            {
+                var multiply = longestWord[i] * shortestWord[i];
+                sum += multiply;
+            }
+
+            for (int i = shortestWord.Length; i < longestWord.Length; i++)
+            {
+                sum += longestWord[i];
+            }
+            return sum;
+        }
+    }
+}
