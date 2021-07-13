fef756d094176ac44100f29a3fe44e6d1dcc5679
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-07-13 12:29:55 +0300

    Programming-Fundamentals

---
+++ TextProcessingExercise/CaesarCipher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            foreach (char item in input)
+            {
+                var currChar = (char)(item + 3);
+                Console.Write(currChar);
+            }
+        }
+    }
+}
